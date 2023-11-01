956e811726295f616ea0f1e8342e2542323fdd99
svanik-gantori <119661803+svanik-gantori@users.noreply.github.com>
2023-11-01 16:26:48 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
+}
+function modelLoaded()
+{
+    console.log('Model is loaded');
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        console.log('Nose x position: '+results[0].pose.nose.x);
+        console.log('Nose y position: '+results[0].pose.nose.y);
+    }
+
+    
+    
+}
+
+function draw()
+{
+    image(video,0,0,300,300);
+}
+
+function take_snapshot()
+{
+    save('myfilteredimage.png');
+}
